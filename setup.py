234f0446246f15a6312ad8d365f387fdfa9429e3
Lnvictor <Lnvictor@github.com>
2021-03-25 22:37:45 -0300

    Fixing

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 README = os.path.join(os.path.dirname(__file__), 'README.md')
 
 setup(name='zeroncy',
-      version='0.1',
+      version='0.4',
       description='Zero dependency lib for python',
       long_description=open(README).read(),
       long_description_content_type='text/markdown',
